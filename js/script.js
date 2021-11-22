34656c2db0286d59ae43312a856e84dbb0230ab0
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-22 16:40:03 +0100

    fine esercizio

--- js/script.js
+++ js/script.js
@@ -15,29 +15,42 @@ const app = new Vue({
     mail: '',
     caricamento: true,
     httpError: false,
+    mails:[],
 
   },
 
   methods:{
 
+    getMail(){
+
+      this.caricamento = true;
+
+      for (let i = 0; i < 10; i++) {
+
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response)=>{
+          console.log('Oggetto',response.data);
+
+          this.mail = response.data.response;
+
+          this.mails.push(this.mail);
+
+          this.caricamento = false;
+        })
+        .catch((error)=>{
+          console.log(error);
+
+          this.httpError = true;
+        })
+      }
+
+    },
+
   },
 
   mounted(){
 
-    //carico axios
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((response)=>{
-      console.log('Oggetto',response.data);
-
-      this.mail = response.data.response;
-
-      this.caricamento = false;
-    })
-    .catch((error)=>{
-      console.log(error);
-
-      this.httpError = true;
-    })
-
+    this.getMail();
+    
   },
 })
